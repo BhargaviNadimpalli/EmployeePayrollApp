ed181bae6f7828386dc7883b10b1e69482fc2ce4
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-09-27 14:38:11 -0600

    Added employee payroll app on submit alert

---
+++ pages/payroll.js
@@ -0,0 +1,50 @@
+class EmployeepayrollData {
+
+    get id() { return this._id; }
+    set id(id) {
+        this._id = id;
+    }
+
+get name() { return this._name; }
+    set name(name) { 
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (nameRegex.test(name))
+           this._name = name;
+        else 
+            throw 'Name is Incorrect!';
+     }
+     get profilePic() { return this._profilePic; }
+     set profilePic(profilePic) {
+         this._profilePic = profilePic;
+     }
+     get gender() { return this._gender; }
+     set gender(gender) {
+         this._gender = gender;
+     }
+     get department() { return this._department; }
+     set department(department) {
+         this._department = department;
+     }
+     get salary() { return this._salary; }
+     set salary(salary) {
+         this._salary = salary;
+     }
+     get note() { return this._note; }
+     set note(note) {
+         this._note = note;
+     }
+     get startDate() { return this._startDate; }
+     set startDate(startDate) {
+         this._startDate = startDate;
+     }
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" :
+                        this.startDate.toLocalDateString("en-US", options);
+    
+        return "id=" + this._id + ", name=" + this._name + ", salary=" + this._salary + ", " + "gender="
+        + this._gender + ", profilePic='" +this._profilePic + ", department=" + this._department +
+        ", startDate=" + this._startDate + ", Note=" + this._note;
+    }
+    
+}
